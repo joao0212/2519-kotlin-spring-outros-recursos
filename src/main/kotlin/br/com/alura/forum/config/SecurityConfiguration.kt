79f2f9a99a2938a6a4a870b455966c17346a9a90
Joao Victor <jvc.martins12@gmail.com>
2022-05-17 23:03:02 -0300

    codigo da primeira aula

--- src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -27,8 +27,6 @@ class SecurityConfiguration(
         csrf()?.disable()?.
         authorizeRequests()?.
         antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
-        antMatchers("/respostas")?.hasAuthority("LEITURA_ESCRITA")?.
-        antMatchers(HttpMethod.GET,"/relatorios")?.hasAuthority("ADMIN")?.
         antMatchers(HttpMethod.POST,"/login")?.permitAll()?.
         antMatchers(HttpMethod.GET, "/swagger-ui/*")?.permitAll()?.
         antMatchers(HttpMethod.GET,"/v3/api-docs/**")?.permitAll()?.
--- src/main/kotlin/br/com/alura/forum/dto/Email.kt
+++
@@ -1,7 +0,0 @@
-package br.com.alura.forum.dto
-
-data class Email(
-    val subject: String,
-    val text: String,
-    val targetEmail: String
-)
